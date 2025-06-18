476b72ece91dc9a0241a8ebce04c787df63206f9
momon-stars <driftmax@outlook.jp>
2025-06-19 06:32:38 +0900

    再アップロード

--- kadai_011/foreach.php
+++ kadai_011/foreach.php
@@ -8,7 +8,7 @@
 
 <body>
   <?php
-  $score = ['Id'=>1,'名前'=>'侍太郎','年齢'=>30];
+  $score = ['名前'=>'玉ねぎ','値段'=>200,'産地'=>'北海道'];
   foreach ($score as $key=>$value){
     echo "{$key}:{$value}<br>";
   }
