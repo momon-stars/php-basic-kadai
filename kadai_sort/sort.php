0f2daa299f55f128ed2fe01c174d0a8091728af5
momon-stars <driftmax@outlook.jp>
2025-06-20 11:35:29 +0900

    アップロード

---
+++ kadai_sort/sort.php
@@ -0,0 +1,51 @@
+<?php
+// 型宣言と異なるデータ型の場合にエラーを発生させる
+declare(strict_types=1);
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>kadai_sort</title>
+</head>
+
+<body>
+
+    <?php
+      // $nums = [15,4,18,23,10];
+      // echo '昇順にソートします。<br>';
+      // asort($nums);
+      // foreach($nums as $value){
+      //   echo $value . '<br>';
+      // }
+      // echo '降順にソートします。<br>';
+      // arsort($nums);
+      // foreach($nums as $value){
+      //   echo $value . '<br>';
+      // }
+      function sort_2way($order){
+        //  ソートする配列を宣言
+        $array = [15, 4, 18, 23, 10 ];
+        if($order){
+          echo'昇順<br>';
+          asort($array);
+        } else {
+          echo"降順";
+          arsort($array);
+        }
+
+        foreach($array as $value){
+          echo $value.'<br>';
+        }
+      }
+      sort_2way(true);
+      sort_2way(false);
+    ?>
+</body>
+
+</html>
+</body>
+
+</html>
